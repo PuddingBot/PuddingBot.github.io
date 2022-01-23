b3fb0ca0d9f7336845f504b71ac242930f050f7f
Happy-Ferret <MarkBauer@gmx.ch>
2022-01-23 12:44:57 +0100

    Homepage: Use correct favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/logo.png',
   organizationName: 'PuddingBot', // Usually your GitHub org/user name.
   projectName: 'PuddingBot.github.io', // Usually your repo name.
   trailingSlash: false,
